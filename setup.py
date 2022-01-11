122c00f77a94c860a86d78f0502197c1c699258f
Dan Welsh <danielwelsh@gmail.com>
2022-01-11 13:15:46 -0500

    re-arranged added setup requirements and readme to structure pas package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='census_geocoder',
+   version='1.0',
+   description='Python wrapper to *some* of the functionality of the US Census Bureau Geocoding web service',
+   author='Dan Welsh',
+   author_email='danielwelsh@gmail.com',
+   packages=['census_geocoder'],  #same as name
+   install_requires=['json','requests'], #external packages as dependencies
+)
