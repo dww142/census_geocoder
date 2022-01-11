c15daab9a7f3cb67789df4c2a0042e4de84e662b
Dan Welsh <danielwelsh@gmail.com>
2022-01-11 13:07:43 -0500

    renamed

---
+++ census_geocoder.py
@@ -0,0 +1,97 @@
+'''
+Python wrapper for the US Census Bureau Geocoding webservice.
+
+Documentation:
+http://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.pdf
+
+TODO: Separate 'Set address' from init, allow print available layers before
+    setting address
+TODO: Move option to set geography layers out of init (to the geocode function ?)
+TODO: Make getting geography layers optional..? for performance when looking for lat/long only
+'''
+import json
+import requests
+import settings
+
+
+class Geocoder:
+    '''
+    Default Layers: 
+        Unified SD, Sec SD, Elem SD
+        , 115th US House Congressional Dist
+        , state house upper (senate)
+        , state house lower (House)
+    '''
+    DEFAULT_LAYERS = ['14', '16', '18', '54', '56', '58']
+    def __init__(self, street, city, state, zip_code=None, layers=DEFAULT_LAYERS):
+        '''
+        '''
+        self.address = {
+            'street' : street,
+            'city' : city,
+            'state' : state,
+            }
+
+        if zip_code:
+            self.address['zip'] = zip_code
+
+        self.layers = layers
+        self.layer_names = [settings.AVAILABLE_LAYERS[l] for l in self.layers]
+
+        self.search_parameters = {
+            'benchmark' : settings.BENCHMARK,
+            'format' : settings.FORMAT,
+            'vintage' : settings.VINTAGE,
+            'layers' : ','.join(self.layers)
+            }
+        # result placeholders
+        self.match_found = False
+        self.matched_address = None
+        self.latitude = None
+        self.longitude = None
+        self.geographies = {}
+
+    def geocode(self):
+        '''
+        TODO: TESTS:
+        TODO: Check/clean Required options / parameters
+        TODO: validate parameters (numeric zip, 2 digit state, non-po box)
+        TODO: Handle request call exception
+        TODO: Check request call result
+        TODO: handle bad JSON exception on JSON.loads
+        TODO: result parser for good json (...)
+        '''
+        request_payload = {**self.address, **self.search_parameters}
+
+        req = requests.get(settings.BASE_URL, params=request_payload)
+
+        if req.ok:
+            result = json.loads(req.content)
+        else:
+            result = json.loads({'result':'NA'})
+        # print(json.dumps(result, indent=4))
+        self.parse_geocode(result.get('result').get('addressMatches')[0])
+        self.parse_geographies(result.get('result').get('addressMatches')[0].get('geographies'))   
+
+    def parse_geocode(self, address_matches):
+        '''
+        Extract latitude, longitude, and matched address from the Geocoder response
+        '''
+        if address_matches:
+            self.latitude = address_matches.get('coordinates').get('y')
+            self.longitude = address_matches.get('coordinates').get('x')
+            self.matched_address = address_matches.get('matchedAddress')
+    
+    def parse_geographies(self, geographies):
+        '''
+        Extract all requested geography layers from request into a dictionary. 
+        '''
+        for layer in self.layer_names:
+            if geographies.get(layer):
+                self.geographies[layer+' GEOID'] = geographies.get(layer)[0].get('GEOID')
+                self.geographies[layer] = geographies.get(layer)[0].get('NAME')
+
+    def available_geography_layers(self):
+        for layer in settings.AVAILABLE_LAYERS:
+            print('Layer Name:', settings.AVAILABLE_LAYERS[layer], '; Layer Code:', layer)
+        print('Default Layers (& Format):', self.DEFAULT_LAYERS)
---
+++ settings.py
@@ -0,0 +1,118 @@
+"""
+A wrapper module for accessing the US Census Bureau's Geocoding API
+
+Benchmarks and Vintages:
+    The benchmark is the time period when we created a snapshot of our data
+    (generally done twice a year).
+    For example, Public_AR_Census2010 is the snapshot we took of the database in 2010.
+    Public_AR_Current is the most recent snapshot we took of our dataset. (THIS IS DEFAULT)
+
+    The vintage of geography is the census or survey that the data relates to.
+     For example, Census2010_Census2010 are the address ranges from the 2010 Census
+        at the time of the 2010 Census.
+     You can also obtain the 2010 Census address ranges as of our most recent benchmark.
+     The vintages you see available depends on the benchmark you selected.
+"""
+
+# required parameters for single address call geocoding (lat/long)
+RETURN_TYPE = 'json'
+SEARCH_TYPE = 'geographies/address'
+# SEARCH_TYPE = 'locations/onelineaddress'
+BENCHMARK = 'Public_AR_Current'
+#required for geographies
+VINTAGE = 'Current_Current'
+BASE_URL = f'https://geocoding.geo.census.gov/geocoder/{SEARCH_TYPE}'
+
+FORMAT = 'json'
+
+AVAILABLE_LAYERS = {
+    '0' : 'Census Public Use Microdata Areas',
+    '1' : '2010 Census Public Use Microdata Areas Labels',
+    '2' : '2010 Census ZIP Code Tabulation Areas',
+    '3' : '2010 Census ZIP Code Tabulation Areas Labels',
+    '4' : 'Tribal Census Tracts',
+    '5' : 'Tribal Census Tracts Labels',
+    '6' : 'Tribal Block Groups',
+    '7' : 'Tribal Block Groups Labels',
+    '8' : 'Census Tracts',
+    '9' : 'Census Tracts Labels',
+    '10' : 'Census Block Groups',
+    '11' : 'Census Block Groups Labels',
+    '12' : '2010 Census Blocks',
+    '13' : '2010 Census Blocks Labels',
+    '14' : 'Unified School Districts',
+    '15' : 'Unified School Districts Labels',
+    '16' : 'Secondary School Districts',
+    '17' : 'Secondary School Districts Labels',
+    '18' : 'Elementary School Districts',
+    '19' : 'Elementary School Districts Labels',
+    '20' : 'Estates',
+    '21' : 'Estates Labels',
+    '22' : 'County Subdivisions',
+    '23' : 'County Subdivisions Labels',
+    '24' : 'Subbarrios',
+    '25' : 'Subbarrios Labels',
+    '26' : 'Consolidated Cities',
+    '27' : 'Consolidated Cities Labels',
+    '28' : 'Incorporated Places',
+    '29' : 'Incorporated Places Labels',
+    '30' : 'Census Designated Places',
+    '31' : 'Census Designated Places Labels',
+    '32' : 'Alaska Native Regional Corporations',
+    '33' : 'Alaska Native Regional Corporations Labels',
+    '34' : 'Tribal Subdivisions',
+    '35' : 'Tribal Subdivisions Labels',
+    '36' : 'Federal American Indian Reservations',
+    '37' : 'Federal American Indian Reservations Labels',
+    '38' : 'Off-Reservation Trust Lands',
+    '39' : 'Off-Reservation Trust Lands Labels',
+    '40' : 'State American Indian Reservations',
+    '41' : 'State American Indian Reservations Labels',
+    '42' : 'Hawaiian Home Lands',
+    '43' : 'Hawaiian Home Lands Labels',
+    '44' : 'Alaska Native Village Statistical Areas',
+    '45' : 'Alaska Native Village Statistical Areas Labels',
+    '46' : 'Oklahoma Tribal Statistical Areas',
+    '47' : 'Oklahoma Tribal Statistical Areas Labels',
+    '48' : 'State Designated Tribal Statistical Areas',
+    '49' : 'State Designated Tribal Statistical Areas Labels',
+    '50' : 'Tribal Designated Statistical Areas',
+    '51' : 'Tribal Designated Statistical Areas Labels',
+    '52' : 'American Indian Joint-Use Areas',
+    '53' : 'American Indian Joint-Use Areas Labels',
+    '54' : '115th Congressional Districts',
+    '55' : '115th Congressional Districts Labels',
+    '56' : '2016 State Legislative Districts - Upper',
+    '57' : '2016 State Legislative Districts - Upper Labels',
+    '58' : '2016 State Legislative Districts - Lower',
+    '59' : '2016 State Legislative Districts - Lower Labels',
+    '60' : 'Census Divisions',
+    '61' : 'Census Divisions Labels',
+    '62' : 'Census Regions',
+    '63' : 'Census Regions Labels',
+    '64' : '2010 Census Urbanized Areas',
+    '65' : '2010 Census Urbanized Areas Labels',
+    '66' : '2010 Census Urban Clusters',
+    '67' : '2010 Census Urban Clusters Labels',
+    '68' : 'Combined New England City and Town Areas',
+    '69' : 'Combined New England City and Town Areas Labels',
+    '70' : 'New England City and Town Area Divisions',
+    '71' : 'New England City and Town Area Divisions Labels',
+    '72' : 'Metropolitan New England City and Town Areas',
+    '73' : 'Metropolitan New England City and Town Areas Labels',
+    '74' : 'Micropolitan New England City and Town Areas',
+    '75' : 'Micropolitan New England City and Town Areas Labels',
+    '76' : 'Combined Statistical Areas',
+    '77' : 'Combined Statistical Areas Labels',
+    '78' : 'Metropolitan Divisions',
+    '79' : 'Metropolitan Divisions Labels',
+    '80' : 'Metropolitan Statistical Areas',
+    '81' : 'Metropolitan Statistical Areas Labels',
+    '82' : 'Micropolitan Statistical Areas',
+    '83' : 'Micropolitan Statistical Areas Labels',
+    '84' : 'States',
+    '85' : 'States Labels',
+    '86' : 'Counties',
+    '87' : 'Counties Labels',
+}
+
